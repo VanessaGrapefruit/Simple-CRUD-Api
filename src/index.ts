ae739fb2bb5721b58eedad1450cf932324381d34
Aliaksei Rachkouski <16lexa1602@gmail.com>
2023-01-17 01:53:45 +0300

    feat: some changes

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,11 @@
 import { config } from 'dotenv';
 import { initializeApp } from './app';
-import { UsersSchema } from './services/users-schema';
 import { initWithLoadBalancer } from './load-balancer';
 
 const isDev = process.argv.includes('--dev');
 const useLoadBalancer = process.argv.includes('--multi');
 
 config();
-
-if (isDev) {
-	UsersSchema.initAsync();
-}
 
 if (useLoadBalancer) {
 	initWithLoadBalancer(initializeApp);
